434446f83458699975c1051cb5fc8c2091a95b06
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-04-27 19:28:02 +0200

    Ejer 9 parte 1

---
+++ Ejercicio 9.py
@@ -0,0 +1,19 @@
+#Media aritmética ponderada
+
+#Parte 1: Escribir un algoritmo que calcula la media aritmética de tres números dados.
+Algoritmo media aritmética
+
+Entrada
+    #Los tres numeros
+    n1: REAL
+    n2: REAL
+    n3: REAL
+
+Resultado: REAL
+
+Precondicion
+    n1, n2, n3 ≥ 0
+
+Realizacion
+    Resultado <- (n1 + n2 + n3) / 3
+    
