9b557f51befdd7ac9c491ac97c6041ce520b162b
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-04-27 19:39:46 +0200

    Ejer 10 parte 1

---
+++ Ejercicio 10.py
@@ -0,0 +1,25 @@
+#Area triángulo
+
+#Parte 1: Escribir un algoritmo que calcula el área 
+#de un triángulo del que se da la medida de un lado y la de la altura relativa a este lado.
+
+Algoritmo área triángulo
+
+Entrada
+    l: REAL #lado
+    h: REAL #altura
+
+Resultado: REAL
+
+Precondicion
+    l > 0
+    h > 0
+
+Realizacion
+    Resultado <- (l*h)/2
+
+Poscondicion
+    Reaultado > 0
+
+Fin area triangulo
+
