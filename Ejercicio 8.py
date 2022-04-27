c5ee754855353196b562f2ce109f03ad3ad24e8a
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-04-27 19:02:19 +0200

    Entrada y resultado

---
+++ Ejercicio 8.py
@@ -0,0 +1,13 @@
+#Porcentajes, IVA e inversiones
+
+#Escribir un algoritmo que calcula el precio con todos los impuestos incluidos (TII) 
+#para un precio sin impuestos y un porcentaje de IVA dado.
+
+Entrada:
+    ci: REAL #Coste inicial
+    iva: REAL #Impuesto
+
+Resultado: REAL
+
+Precondicion:
+
