66de0679d9990e298def1e2ef0113d62ed17fed5
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-04-27 19:35:49 +0200

    Ejer 9 acabado

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -12,8 +12,32 @@ Entrada
 Resultado: REAL
 
 Precondicion
-    n1, n2, n3 ≥ 0
+    n1, n2, n3 >= 0
 
 Realizacion
     Resultado <- (n1 + n2 + n3) / 3
-    
+
+Poscondicion
+    Resultado >= 0
+
+Fin media aritmética
+
+#Parte 2: La misma pregunta para una media ponderada cuando se dan los números y los coeficientes de ponderación.
+
+Entrada
+    n1: REAL
+    n2: REAL
+    n3: REAL
+    c1, c2, c3 : REAL #coeficientes de ponderacion
+
+Precondicion
+    n1, n2, n3 >= 0
+    0 < c1, c2, c3 <= 1 
+
+Realizacion
+    Resultado <- n1*c1 + n2*c2 + n3*c3
+
+Poscondicion
+    Resultado >= 0
+
+Fin media aritmetica con coeficientes de ponderacion
