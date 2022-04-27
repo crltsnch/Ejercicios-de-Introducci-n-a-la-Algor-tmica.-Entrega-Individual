09ba2e3f0ccc583a481a3055ef865a2eff927692
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-04-27 19:12:08 +0200

    Parte 1 acabada

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -1,13 +1,36 @@
 #Porcentajes, IVA e inversiones
 
-#Escribir un algoritmo que calcula el precio con todos los impuestos incluidos (TII) 
+#Parte 1: Escribir un algoritmo que calcula el precio con todos los impuestos incluidos (TII) 
 #para un precio sin impuestos y un porcentaje de IVA dado.
 
-Entrada:
+Algoritmo calculo de impuestos
+
+Entrada
     ci: REAL #Coste inicial
     iva: REAL #Impuesto
 
 Resultado: REAL
 
-Precondicion:
+Precondicion
+    ci > 0
+    iva > 0
 
+Variables
+    iva1: REAL #porcentaje del IVA del precio incial
+
+Realizacion
+    iva1 <- ci * (iva / 100) #cálculo del iva del producto sin impuestos
+    Resultado <- ci + iva1 #coste final con iva
+
+Poscondicion
+    Resultado > ci
+
+Fin calculo de impuesto
+
+
+#Parte 2: Escribir un algoritmo que calcula el importe de los intereses generados por un capital 
+#invertido a un interés dado durante un tiempo dado, expresado en meses.
+
+Algoritmo importe
+
+Entrada
